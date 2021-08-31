3c89cc0db02642badf837fbc8680e8712eb0440b
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-31 16:44:45 +0100

    Route decorator for home page

--- app.py
+++ app.py
@@ -10,3 +10,32 @@ def get_db_con():
     con = sqlite3.connect('database.db')
     con.row_factory = sqlite3.Row
     return con
+
+
+@app.route('/', methods=('GET', 'POST'))
+def home():
+    con = get_db_con()
+
+    #add conditional statements to handle requests
+    if request.method == 'POST':
+        url = request.form['url'] #This gets url form the request and assigns to the variable
+
+        if url is None:
+            flash('PLease provide the URL')
+            return redirect(url_for('home'))
+
+        old_url = con.execute('INSERT INTO urls (original_url) VALUES (?)',
+                                (url,))
+
+        con.commit()
+        con.close()
+
+        url_id = old_url.lastrowid #lastrowid attribute allows for the id of the most recent entry into the table
+        hashid = hashids.encode(url_id)
+        shorterned_url = request.host_url + hashid
+
+        return render_tempplate('home.html', shorterned_url=shorterned_url)
+
+    return render_template('home.html')        
+
+        
