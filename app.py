ee1399935b170d7f6a493d604d4dd19b70cae8df
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-31 16:22:23 +0100

    connection function to database defined

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import sqlite3
 from hashids import Hashids
 from flask import Flask, render_tenplate, request, flash, redirect, url_for
+
+def get_db_connection():
+    con = sqlite3.connect('database.db')
+    con.row_factory = sqlite3.Row
+    return con
